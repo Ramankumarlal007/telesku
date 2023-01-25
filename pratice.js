2bc4b8cc3032cb1274595abf7551e34090255c52
Raman kumar <ramankumarlal@outlook.com>
2023-01-25 17:50:53 +0530

    practising javascript

---
+++ pratice.js
@@ -0,0 +1,43 @@
+// console.log(this);
+// let alien = {
+//     name: 'Navin',
+//     tech: 'js',
+
+
+// let input = 'name'
+// let input = 'tech'
+
+
+// console.log(alien, typeof alien)
+// console.log(alien.name)
+// console.log(alien.tech)
+// console.log(alien['tech']);
+// console.log(alien.input)
+// console.log(alien[input]);
+
+
+
+
+// console.log(alien.laptop?.brand.length);
+
+
+let alien = {
+    name : 'Raman',
+    technology : 'javascript',
+    laptop : {
+        cpu : 'i7',
+        ram : 32,
+        brand : 'lenevo',
+    }
+}
+
+for(let key in alien.laptop)
+console.log(key, alien.laptop[key]);
+
+
+
+
+
+
+
+
